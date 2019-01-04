aabdb115e41ac5f35d8478cf2364642fb5a11cdf
nishantghanate <nishant7.ng@gmail.com>
2019-01-04 18:22:42 +0530

    Pytesseract ocr trained data path added

--- OpticalCharacterRecognition/Ocr.py
+++ OpticalCharacterRecognition/Ocr.py
@@ -5,8 +5,10 @@ import pytesseract
 # First ddiwnload andd install  : windows download : https://github.com/UB-Mannheim/tesseract/wiki
 
 # set file path 
-pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files (x86)\Tesseract-OCR\\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = 'H:\Program Files (x86)\Tesseract-OCR\\tesseract.exe'
 
+# copy : H:\Program Files (x86)\Tesseract-OCRtests\eng.traineddata 
+# paste to : H:\Program Files (x86)\Tesseract-OCR
 # Define config parameters.
 # '-l eng'  for using the English language
 # '--oem 1' sets the OCR Engine Mode to LSTM only.
@@ -28,9 +30,9 @@ imPath = "H:\Github\OpenCv\OpticalCharacterRecognition\Google.png"
 config = ('-l eng --oem 1 --psm 3')
 
 img = cv2.imread(imPath, cv2.IMREAD_COLOR)
-cv2.imshow(" " ,img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.imshow(" " ,img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
 # Run tesseract OCR on image
 text = pytesseract.image_to_string(img, config=config)
